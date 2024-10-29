a0a31d18d375a1e1155a377927a62eb7da48f17c
Md. Imran Hossain <65802128+Imran012x@users.noreply.github.com>
2024-10-30 03:02:59 +0600

    Update app.py

--- app.py
+++ app.py
@@ -8,7 +8,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def index():
-    return "Hello world"
+    return "Welcome to this BOUSST Project"
 
 @app.route('/predict',methods=['POST'])
 def predict():
